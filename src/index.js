49da4cbeaa8f05fc09a1862b5ba10a30fcf6848d
litelp <lponomareva97@yandex.ru>
2024-10-27 21:05:12 +0400

    feat: add solutaion

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let revers = false;
+  let result = [];
+  if (matrix === undefined || matrix[0] === undefined) {
+      return [];
+  }
+
+  matrix.forEach(el => {
+      if (revers === false) {
+          revers = true;
+          if (Array.isArray(el)) return el.forEach(el => result.push(el));
+          result.push(el);
+      } else {
+          revers = false;
+          let a = el.reverse();
+          if (Array.isArray(el)) return a.forEach(el => result.push(el));
+          result.push(el);
+      }
+  })
+
+return result;
 }
